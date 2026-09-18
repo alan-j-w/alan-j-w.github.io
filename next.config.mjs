968cb749fcc58d66ca702ab08fa377ecb60fab28
alan-j-w <alanjoywilson@gmail.com>
2026-09-18 18:24:50 +0530

    Migrate portfolio to user site

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,4 @@
 /** @type {import('next').NextConfig} */
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH ?? "/portfolio";
-
 const nextConfig = {
   output: "export",
   trailingSlash: true,
@@ -9,9 +7,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-
-  basePath,
-  assetPrefix: basePath,
 };
 
 export default nextConfig;
